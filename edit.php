2eafe76182dd3bdb1e1576bb6227b178bb102f41
Brian <Brian@Brian-PC-Asus>
2012-12-12 11:48:01 -0600

    empty edit page

---
+++ edit.php
@@ -0,0 +1,36 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="styles.css">
+<link rel="stylesheet" type="text/css" href="table.css">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<!--<script src="inc/jquery-1.8.2.min.js"></script>-->
+<script type="text/javascript">
+            
+$(window).load(function () {
+});
+    
+$(document).ready(function() {
+        
+  });
+  
+</script>
+
+    </head>
+    <body>
+        <div id="container">
+            <div id="edits">
+
+            </div>
+        </div>
+    </body>
+</html>
+<?php
+   
+  $keyObjects = $_GET["KeyObject"];
+ // $keyObjects->
+        ?>
